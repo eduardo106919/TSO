55bddc0a310827e585a836a3832ddb85951d14db
eduardo106919 <a106919@alunos.uminho.pt>
2026-02-28 14:44:25 +0000

    add guide 03 resolution

---
+++ guides/guide-03/systracer.bpf.c
@@ -0,0 +1,268 @@
+#include "vmlinux.h"
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
+#include <bpf/bpf_core_read.h>
+#include "systracer.h"
+
+struct
+{
+    __uint(type, BPF_MAP_TYPE_RINGBUF);
+    __uint(max_entries, 8 * 1024 * 1024);
+} events SEC(".maps");
+
+struct
+{
+    __uint(type, BPF_MAP_TYPE_ARRAY);
+    __type(key, u32);
+    __type(value, u64);
+    __uint(max_entries, 1);
+} lost_events SEC(".maps");
+
+struct
+{
+    __uint(type, BPF_MAP_TYPE_HASH);
+    __type(key, u32);
+    __type(value, u32);
+    __uint(max_entries, 1);
+} pids SEC(".maps");
+
+struct block_rq_submission
+{
+    uint16_t common_type;
+    uint8_t common_flags;
+    uint8_t common_preempt_count;
+    int32_t common_pid;
+    uint32_t dev;
+    uint64_t sector;
+    uint32_t nr_sectors;
+    uint32_t nr_bytes;
+    uint16_t ioprio;
+    int8_t rwbs[10];
+    int8_t comm[16];
+    int8_t cmd[4];
+};
+
+int to_discard(u32 pid)
+{
+    u32 *value = bpf_map_lookup_elem(&pids, &pid);
+    if (value == NULL)
+    {
+        return 1;
+    }
+    return *value != pid;
+}
+
+int create_and_submit_event(u32 pid, u64 id, u32 type)
+{
+    struct data_t *data = bpf_ringbuf_reserve(&events, sizeof(struct data_t), 0);
+    if (!data)
+    {
+        u32 key = 0;
+        u64 *lost_counter = bpf_map_lookup_elem(&lost_events, &key);
+        if (lost_counter)
+        {
+            __sync_fetch_and_add(lost_counter, 1);
+            return 0;
+        }
+        return 1;
+    }
+
+    data->pid = pid;
+    data->syscall_id = id;
+    data->type = type;
+    bpf_get_current_comm(&data->command, sizeof(data->command));
+
+    bpf_ringbuf_submit(data, 0);
+
+    return 0;
+}
+
+SEC("tp/syscalls/sys_enter_openat")
+int BPF_PROG(sys_enter_openat,
+             struct pt_regs *regs,
+             long syscall_id)
+{
+    char comm[16];
+    bpf_get_current_comm(comm, sizeof(comm));
+    if (__builtin_memcmp(comm, "systracer", 9) == 0)
+    {
+        return 0;
+    }
+
+    u32 pid = bpf_get_current_pid_tgid() >> 32;
+    if (to_discard(pid))
+        return 0;
+    return create_and_submit_event(pid, syscall_id, SYSCALL);
+}
+
+SEC("tp/syscalls/sys_enter_read")
+int BPF_PROG(sys_enter_read,
+             struct pt_regs *regs,
+             long syscall_id)
+{
+    char comm[16];
+    bpf_get_current_comm(comm, sizeof(comm));
+    if (__builtin_memcmp(comm, "systracer", 9) == 0)
+    {
+        return 0;
+    }
+
+    u32 pid = bpf_get_current_pid_tgid() >> 32;
+    if (to_discard(pid))
+        return 0;
+    return create_and_submit_event(pid, syscall_id, SYSCALL);
+}
+
+SEC("tp/syscalls/sys_enter_write")
+int BPF_PROG(sys_enter_write,
+             struct pt_regs *regs,
+             long syscall_id)
+{
+    char comm[16];
+    bpf_get_current_comm(comm, sizeof(comm));
+    if (__builtin_memcmp(comm, "systracer", 9) == 0)
+    {
+        return 0;
+    }
+
+    u32 pid = bpf_get_current_pid_tgid() >> 32;
+    if (to_discard(pid))
+        return 0;
+    return create_and_submit_event(pid, syscall_id, SYSCALL);
+}
+
+SEC("tp/syscalls/sys_enter_close")
+int BPF_PROG(sys_enter_close,
+             struct pt_regs *regs,
+             long syscall_id)
+{
+    char comm[16];
+    bpf_get_current_comm(comm, sizeof(comm));
+    if (__builtin_memcmp(comm, "systracer", 9) == 0)
+    {
+        return 0;
+    }
+
+    u32 pid = bpf_get_current_pid_tgid() >> 32;
+    if (to_discard(pid))
+        return 0;
+    return create_and_submit_event(pid, syscall_id, SYSCALL);
+}
+
+SEC("kprobe/vfs_open")
+int BPF_KPROBE(vfs_enter_open,
+               const struct path *path,
+               struct file *file)
+{
+    u32 pid = bpf_get_current_pid_tgid() >> 32;
+    if (to_discard(pid))
+        return 0;
+    return create_and_submit_event(pid, MYOP_OPENAT, VFS);
+}
+
+SEC("kprobe/vfs_read")
+int BPF_KPROBE(vfs_enter_read,
+               const struct path *path,
+               struct file *file)
+{
+    u32 pid = bpf_get_current_pid_tgid() >> 32;
+    if (to_discard(pid))
+        return 0;
+    return create_and_submit_event(pid, MYOP_READ, VFS);
+}
+
+SEC("kprobe/vfs_write")
+int BPF_KPROBE(vfs_enter_write,
+               const struct path *path,
+               struct file *file)
+{
+    u32 pid = bpf_get_current_pid_tgid() >> 32;
+    if (to_discard(pid))
+        return 0;
+    return create_and_submit_event(pid, MYOP_WRITE, VFS);
+}
+
+SEC("kprobe/ext4_file_open")
+int BPF_KPROBE(ext4_enter_open,
+               struct inode *inode,
+               struct file *filp)
+{
+    u32 pid = bpf_get_current_pid_tgid() >> 32;
+    if (to_discard(pid))
+        return 0;
+    return create_and_submit_event(pid, MYOP_OPENAT, EXT4);
+}
+
+SEC("kprobe/ext4_file_read_iter")
+int BPF_KPROBE(ext4_enter_read,
+               struct kiocb *iocb, struct iov_iter *to)
+{
+    u32 pid = bpf_get_current_pid_tgid() >> 32;
+    if (to_discard(pid))
+        return 0;
+    return create_and_submit_event(pid, MYOP_READ, EXT4);
+}
+
+SEC("kprobe/ext4_file_write_iter")
+int BPF_KPROBE(ext4_enter_write,
+               struct kiocb *iocb, struct iov_iter *from)
+{
+    u32 pid = bpf_get_current_pid_tgid() >> 32;
+    if (to_discard(pid))
+        return 0;
+    return create_and_submit_event(pid, MYOP_WRITE, EXT4);
+}
+
+SEC("tp/block/block_rq_issue")
+int trace_block_rq(struct block_rq_submission *ctx)
+{
+    u32 pid = bpf_get_current_pid_tgid() >> 32;
+    if (to_discard(pid))
+        return 0;
+
+    if (ctx->rwbs[0] == 'R')
+    {
+        return create_and_submit_event(pid, MYOP_READ, BLOCK);
+    }
+    else if (ctx->rwbs[0] == 'W')
+    {
+        return create_and_submit_event(pid, MYOP_WRITE, BLOCK);
+    }
+    return 0;
+}
+
+/*
+SEC("raw_tp/sys_enter")
+int syscall_enter_trace(struct bpf_raw_tracepoint_args *ctx)
+{
+    char comm[16];
+    bpf_get_current_comm(comm, sizeof(comm));
+    if (__builtin_memcmp(comm, "systracer", 9) == 0)
+    {
+        return 0;
+    }
+
+    struct data_t *data = bpf_ringbuf_reserve(&events, sizeof(struct data_t), 0);
+    if (!data)
+    {
+        u32 key = 0;
+        u64 *lost_counter = bpf_map_lookup_elem(&lost_events, &key);
+        if (lost_counter)
+        {
+            __sync_fetch_and_add(lost_counter, 1);
+            return 0;
+        }
+        return 1;
+    }
+
+    data->pid = bpf_get_current_pid_tgid() >> 32;
+    data->syscall_id = ctx->args[1];
+    bpf_get_current_comm(&data->command, sizeof(data->command));
+
+    bpf_ringbuf_submit(data, 0);
+
+    return 0;
+}
+*/
+
+char LICENSE[] SEC("license") = "Dual BSD/GPL";
---
+++ guides/guide-03/systracer.h
@@ -0,0 +1,27 @@
+#ifndef SYSTRACER_H
+#define SYSTRACER_H
+
+struct data_t
+{
+   int pid;
+   char command[16];
+   long syscall_id;
+   int type;
+};
+
+enum type_e
+{
+   SYSCALL,
+   VFS,
+   EXT4,
+   BLOCK,
+};
+
+enum my_op
+{
+   MYOP_OPENAT = 2,
+   MYOP_READ = 0,
+   MYOP_WRITE = 1,
+};
+
+#endif
